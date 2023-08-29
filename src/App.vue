10bcb0dd42c552d2c9cf4d99a6a2e82c3cd1f31c
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-29 17:55:26 -0400

    components made

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,18 @@
 
 <template>
   <div>
-    <h1>my new portfolio</h1>
+    <h1>App will be the landing page</h1>
   </div>
 </template>
 
 <style scoped>
+
+h1 {
+  text-align: center;
+  font-size: 25px;
+  font-weight: bold;
+  background-color: white;
+}
+
+
 </style>
