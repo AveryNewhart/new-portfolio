f9961df573e8e3d7461f9475960567ec83eebf1e
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-30 19:17:20 -0400

    components being called in layout

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,12 @@ import Background from './components/Background.vue';
   <div id="app">
     <Background />
     <div class="componentDiv">
-      <h1>call components in here</h1>
+      <h1>component nav</h1>
+      <h1>component about</h1>
+      <h1>component porjects</h1>
+      <h1>component resume</h1>
+      <h1>component contact</h1>
+      <h1>component footer</h1>
     </div>
   </div>
 </template>
