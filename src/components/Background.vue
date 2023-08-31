b852f2d5349db4165fdeefa97c247679c401fdb6
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-31 17:41:27 -0400

    changed colors in background

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -24,7 +24,7 @@
   }
   
   .wave {
-    background: rgb(255 255 255 / 25%);
+    background: rgba(0, 0, 0, 0.25);
     border-radius: 1000% 1000% 0 0;
     position: absolute;
     width: 200%;
@@ -72,7 +72,7 @@
       rgba(101, 0, 94, 1) 3%,
       rgba(60, 132, 206, 1) 38%,
       rgba(48, 238, 226, 1) 68%,
-      rgba(255, 25, 25, 1) 98%
+      rgb(29, 255, 25) 98%
     );
     animation: gradient 15s ease infinite;
     background-size: 400% 400%;
