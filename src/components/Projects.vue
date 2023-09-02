7a570efc6dd19b0aec1be63263af34ebd40d17e8
AveryNewhart <newhartreeceavery@gmail.com>
2023-09-02 14:24:54 -0400

    carousel can handle up to 15 items, i want to change it from bubbles to left/right arrows

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -45,13 +45,13 @@ const frontends: Frontend[] = [
         about: 'i love math this is the about section',
         repo: 'put repo link',
     },
-    {
-        title: 'Free Blackjack',
-        image: 'put an under construction picture here',
-        language: 'languages used here, js',
-        about: 'i love blackjack this is the about section',
-        repo: 'put repo link',
-    },
+    // {
+    //     title: 'Free Blackjack',
+    //     image: 'put an under construction picture here',
+    //     language: 'languages used here, js',
+    //     about: 'i love blackjack this is the about section',
+    //     repo: 'put repo link',
+    // },
     {
         title: 'Note Taker',
         image: '/images/noteTaker.png',
@@ -109,6 +109,8 @@ const frontends: Frontend[] = [
         repo: 'put repo link',
     },
 ];
+
+const totalFrontendProjects = ref(frontends.length);
 
 interface Backend {
     title: string;
@@ -205,7 +207,7 @@ const fullstacks: Fullstack[] = [
     <h3>Frontend</h3>
       <div class="bigBody">
         <input type="radio" name="position" v-for="(frontend, index) in frontends" :key="frontend.title" :id="'position-' + index" @click="updatePosition(index)" />
-        <main id="carousel">
+        <main id="carousel" :style="{ '--total-frontend-projects': totalFrontendProjects }">
           <div class="item" v-for="(frontend, index) in frontends" :key="frontend.title" :class="{ active: index === currentPosition }">
             <div class="projectDiv">
               <div class="projectContent">
@@ -305,7 +307,7 @@ main#carousel {
   overflow: hidden;
   transform-style: preserve-3d;
   perspective: 600px;
-  --items: 5;
+  --items: 13;
   --middle: 3;
   --position: 1;
   pointer-events: none;
@@ -343,6 +345,46 @@ div.item:nth-of-type(4) {
 }
 div.item:nth-of-type(5) {
   --offset: 5;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(6) {
+  --offset: 6;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(7) {
+  --offset: 7;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(8) {
+  --offset: 8;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(9) {
+  --offset: 9;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(10) {
+  --offset: 10;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(11) {
+  --offset: 11;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(12) {
+  --offset: 12;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(13) {
+  --offset: 13;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(14) {
+  --offset: 14;
+  background-color: #e9ff70;
+}
+div.item:nth-of-type(15) {
+  --offset: 15;
   background-color: #e9ff70;
 }
 
@@ -384,6 +426,96 @@ input:nth-of-type(5) {
 }
 input:nth-of-type(5):checked ~ main#carousel {
   --position: 5;
+}
+
+input:nth-of-type(6) {
+  grid-column: 7 / 8;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(6):checked ~ main#carousel {
+  --position: 6;
+}
+
+input:nth-of-type(7) {
+  grid-column: 8 / 9;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(7):checked ~ main#carousel {
+  --position: 7;
+}
+
+input:nth-of-type(8) {
+  grid-column: 9 / 10;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(8):checked ~ main#carousel {
+  --position: 8;
+}
+
+input:nth-of-type(9) {
+  grid-column: 10 / 11;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(9):checked ~ main#carousel {
+  --position: 9;
+}
+
+input:nth-of-type(10) {
+  grid-column: 11 / 12;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(10):checked ~ main#carousel {
+  --position: 10;
+}
+
+input:nth-of-type(11) {
+  grid-column: 12 / 13;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(11):checked ~ main#carousel {
+  --position: 11;
+}
+
+input:nth-of-type(12) {
+  grid-column: 13 / 14;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(12):checked ~ main#carousel {
+  --position: 12;
+}
+
+input:nth-of-type(13) {
+  grid-column: 14 / 15;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(13):checked ~ main#carousel {
+  --position: 13;
+}
+
+input:nth-of-type(14) {
+  grid-column: 15 / 16;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(14):checked ~ main#carousel {
+  --position: 14;
+}
+
+input:nth-of-type(15) {
+  grid-column: 16 / 17;
+  grid-row: 2 / 3;
+}
+
+input:nth-of-type(15):checked ~ main#carousel {
+  --position: 15;
 }
 
 </style>
