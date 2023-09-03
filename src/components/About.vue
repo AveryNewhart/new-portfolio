3a8ef40b293179a836b851c7a451cdb4c266f36d
AveryNewhart <newhartreeceavery@gmail.com>
2023-09-03 18:49:43 -0400

    basic layout created for the about me page

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,64 @@
 <template>
   <div class="aboutMeDiv">
     <h1>About</h1>
+    <div class="aboutContent">
+      <div class="aboutText">
+        <p>WHO AM I?</p>
+        <p>Text about myself goes here.</p>
+      </div>
+      <img class="myPic" src='/images/myPic.png'>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.aboutMeDiv {
+  border: 2px solid red;
+  padding: 20px;
+  height: 900px;
+}
+
+.aboutContent {
+  display: flex;
+  flex-direction: row;
+  align-items: center; 
+}
+
+.aboutText {
+  flex: 1; 
+  padding: 20px;
+  background-color: rgba(0, 0, 0, 0.1); 
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Shadow effect */
+  border-radius: 8px;
+}
+
+.myPic {
+  height: 350px;
+  width: 350px;
+  margin-left: 20px;
+  border-radius: 8px; 
+}
+
+@media (max-width: 768px) {
+  .aboutContent {
+    flex-direction: column;
+  }
+  .myPic {
+    margin-left: 0;
+    margin-top: 20px;
+  }
+}
+
+</style>
+
+
+<!-- <template>
+  <div class="aboutMeDiv">
+    <h1>About</h1>
     <p>WHO AM I?</p>
+    <p>text about myself is going to go here and will be the left side of the screen, like i have it explained in the commented out code</p>
+    <img class="myPic" src='/images/myPic.png'>
     <p>i want the text to be on the left side of the screen on desktop</p>
     <p>i want the image to be on the right side of the screen when on desktop but then when on mobile it will be above the about me section</p>
     <p>i want the image to have cool effects of a cool background to it, make it subtle but stand out.</p>
@@ -19,4 +76,9 @@
   border: 2px solid red;
 }
 
-</style>
+.myPic {
+  height: 350px;
+  width: 350px;
+}
+
+</style> -->
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/new-portfolio/',
   plugins: [vue()],
