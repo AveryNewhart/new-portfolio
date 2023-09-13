22f66c4244b9b9dd6a22015e604ddcf61a2d6fa9
AveryNewhart <newhartreeceavery@gmail.com>
2023-09-12 20:54:33 -0400

    working on styling navbar buttons

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -75,16 +75,16 @@ methods: {
           "
         >
           <li class="text-sm font-bold hover:text-blue-400">
-            <a @click="navigateTo('about1')" class="">About</a>
+            <a @click="navigateTo('about1')" class="navBut">About</a>
           </li>
           <li class="text-sm font-bold hover:text-blue-400">
-            <a @click="navigateTo('projects1')" class="">Projects</a>
+            <a @click="navigateTo('projects1')" class="navBut">Projects</a>
           </li>
           <li class="text-sm font-bold hover:text-blue-400">
-            <a @click="navigateTo('resume1')" class="">Resume</a>
+            <a @click="navigateTo('resume1')" class="navBut">Resume</a>
           </li>
           <li class="text-sm font-bold hover:text-blue-400">
-            <a @click="navigateTo('contact1')" class="">Contact</a>
+            <a @click="navigateTo('contact1')" class="navBut">Contact</a>
           </li>
         </ul>
       </nav>
@@ -96,6 +96,94 @@ methods: {
 
 .mainDivNav {
   border-bottom: 5px solid black;
+}
+
+.navBut {
+  padding: 10px;
+  font: 25px;
+  color:#ffff;
+  border-radius:20px;
+  background-image: linear-gradient(40deg,#d38cff,#6272fc);
+  box-shadow: rgba(50, 50, 93, 1) 0px 50px 100px -20px, rgba(0, 0, 0, 0.5) 0px 30px 60px -30px, rgba(10, 37, 64, 0.65) 0px -2px 6px 0px inset;
+  cursor:pointer;
+  transform: scale(0.8);
+  transition: all .2s ease-out;
+}
+
+.navBut:hover{
+  background-image: linear-gradient(30deg,#f5b6b3,#6272fc);
+  transform: scale(0.9);
+  transition-duration: 1s ease-out;
+  box-shadow: #030557 0px 25px 35px -13px;
+  border-radius:15px;
+  color:#ffffeb;
+  border:0.1px  #6e0175;
+  -webkit-tap-highlight-color: transparent;
+  animation-play-state: paused;
+}
+.navBut:after{
+  animation-play-state: paused;
+}
+
+@keyframes shine {
+  0% {
+    transform: translateX(-30px) rotate(-25deg);
+  }
+  
+  100% {
+    transform: translateX(250px) rotate(-25deg);
+  }
+}
+
+
+
+
+
+
+@media only screen and (max-width:600px){
+  body{
+  background-color:white;
+}
+.navBut{
+  list-style:none;
+  margin: 0px;
+  padding: 10px 20px;
+  font-size: 0.8rem;
+  font-family:bolder;
+  font-family:"Monaco";
+  color:#ffff;
+  border-radius:16px;
+  background-image: linear-gradient(40deg,#d38cff,#6272fc);
+  box-shadow: rgba(50, 50, 93, 1) 0px 50px 100px -20px, rgba(0, 0, 0, 0.5) 0px 30px 60px -30px, rgba(10, 37, 64, 0.65) 0px -2px 6px 0px inset;
+  cursor:pointer;
+  transform: scale(0.8);
+  transition: all .2s ease-in-out;
+}
+.navBut{
+  display:flex;
+  justify-content:center;
+}
+.navBut{
+  margin-top:5rem;
+  margin-left:-2rem
+}
+
+.navBut:hover{
+  background-image: linear-gradient(30deg,#f5b6b3,#6272fc);
+  font-size: 0.8rem;
+  transform: scale(0.9);
+  transition-duration: 1s ease-out;
+  box-shadow: #030557 0px 25px 35px -5px;
+  border-radius:13px;
+  color:#ffffeb;
+  border:0.1px  #6e0175;
+  animation-play-state: paused;
+/*   font-family:"Monaco" */
+}
+  .navBut:hover::after{
+    animation-play-state: paused;
+  }
+
 }
 
 </style>
