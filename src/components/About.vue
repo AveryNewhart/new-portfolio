fe4542eceecbc5567a3d75a02255f984b0003e4d
AveryNewhart <newhartreeceavery@gmail.com>
2023-09-15 18:36:10 -0400

    projects button in nav is a dropdown for all project types

--- src/components/About.vue
+++ src/components/About.vue
@@ -71,30 +71,3 @@ h1 {
 
 </style>
 
-
-<!-- <template>
-  <div class="aboutMeDiv">
-    <h1>About</h1>
-    <p>WHO AM I?</p>
-    <p>text about myself is going to go here and will be the left side of the screen, like i have it explained in the commented out code</p>
-    <img class="myPic" src='/images/myPic.png'>
-    <p>i want the text to be on the left side of the screen on desktop</p>
-    <p>i want the image to be on the right side of the screen when on desktop but then when on mobile it will be above the about me section</p>
-    <p>i want the image to have cool effects of a cool background to it, make it subtle but stand out.</p>
-    <p>for the background of the text have a nice subtle darker type of background with some nice shadow effects/lights to go well with the background of the site.</p>
-    <p>look at "ideas" in my discord channel to get a guide of how i want to do it.</p>
-  </div>
-</template>
-
-<style scoped>
-
-.aboutMeDiv {
-  border: 2px solid red;
-}
-
-.myPic {
-  height: 350px;
-  width: 350px;
-}
-
-</style> -->
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -5,12 +5,16 @@ export default defineComponent({
   data() {
     return {
       showMenu: false,
+      dropdownOpen: false,
     };
   },
 
 methods: {
     navigateTo(sectionId: string) {
       this.$emit('navigate', sectionId); // Emit a custom event with the sectionId
+    },
+    toggleDropdown() {
+      this.dropdownOpen = !this.dropdownOpen;
     },
   },
 });
@@ -78,8 +82,41 @@ methods: {
           <li class="text-sm font-bold hover:text-blue-400">
             <a @click="navigateTo('about1')" class="navBut">About</a>
           </li>
-          <li class="text-sm font-bold hover:text-blue-400">
+          <!-- <li class="text-sm font-bold hover:text-blue-400">
             <a @click="navigateTo('projects1')" class="navBut">Projects</a>
+          </li> -->
+          <li class="group text-sm font-bold hover:text-blue-400">
+            <button @click="toggleDropdown" class="navBut dropdown-button">
+              Projects
+              <svg
+                class="w-4 h-4 ml-2 transform group-hover:rotate-180 transition-transform"
+                :class="{ 'rotate-180': dropdownOpen }"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <!-- Dropdown menu -->
+            <ul
+              v-if="dropdownOpen"
+              class="mt-2 space-y-2 bg-white border border-gray-200 py-2 px-4 rounded-md shadow-lg"
+            >
+              <li>
+                <a @click="navigateTo('projects1')">Frontend</a>
+              </li>
+              <li>
+                <a @click="navigateTo('backend1')">Backend</a>
+              </li>
+              <li>
+                <a @click="navigateTo('fullstack1')">Fullstack</a>
+              </li>
+            </ul>
           </li>
           <li class="text-sm font-bold hover:text-blue-400">
             <a @click="navigateTo('resume1')" class="navBut">Resume</a>
@@ -144,6 +181,40 @@ methods: {
 }
 
 
+.dropdown-button {
+  padding: 10px 20px;
+  font-size: 0.8rem;
+  font-family: bolder;
+  font-family: "Monaco";
+  border-radius: 16px;
+  background-image: linear-gradient(315deg, rgb(29, 255, 25) 3%, rgba(48, 238, 226, 1) 38%, rgba(60, 132, 206, 1) 68%, rgba(101, 0, 94, 1) 98%); 
+  box-shadow: rgba(50, 50, 93, 1) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.5) 0px 30px 60px -30px, rgba(10, 37, 64, 0.65) 0px -2px 6px 0px inset;
+  cursor: pointer;
+  transform: scale(0.8);
+  transition: all 0.2s ease-in-out;
+}
+
+.dropdown-button:hover {
+  background-image: linear-gradient(30deg, #f5b6b3, #6272fc);
+  font-size: 0.8rem;
+  transform: scale(0.9);
+  transition-duration: 1s ease-out;
+  box-shadow: #030557 0px 25px 35px -5px;
+  border-radius: 13px;
+  color: #ffffeb;
+  border: 0.1px #6e0175;
+  animation-play-state: paused;
+}
+
+.dropdown-button:hover::after {
+  animation-play-state: paused;
+}
+
+.dropdown-button + ul {
+  width: 100%;
+}
+
 
 
 
@@ -159,9 +230,9 @@ methods: {
   font-size: 0.8rem;
   font-family:bolder;
   font-family:"Monaco";
-  color:#ffff;
+  color:black;
   border-radius:16px;
-  background-image: linear-gradient(40deg,#d38cff,#6272fc);
+  background-image: linear-gradient(315deg, rgb(29, 255, 25) 3%, rgba(48, 238, 226, 1) 38%, rgba(60, 132, 206, 1) 68%, rgba(101, 0, 94, 1) 98%); 
   box-shadow: rgba(50, 50, 93, 1) 0px 50px 100px -20px, rgba(0, 0, 0, 0.5) 0px 30px 60px -30px, rgba(10, 37, 64, 0.65) 0px -2px 6px 0px inset;
   cursor:pointer;
   transform: scale(0.8);
@@ -191,6 +262,7 @@ methods: {
   .navBut:hover::after{
     animation-play-state: paused;
   }
+
 
 }
 
