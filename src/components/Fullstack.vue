2c39ea373b0fbff6d8696385fe508caa677e7b28
AveryNewhart <newhartreeceavery@gmail.com>
2023-09-30 16:18:37 -0400

    changing project pages from being carousels

--- src/components/Fullstack.vue
+++ src/components/Fullstack.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
 
-import { ref } from 'vue';
-
-const currentPosition = ref(0);
-
-const updatePosition = (index: number) => {
-  currentPosition.value = index;
-};
 
 interface Fullstack {
     title: string;
@@ -55,253 +48,102 @@ const fullstacks: Fullstack[] = [
 </script>
 
 <template>
-  <div class="mainProjDiv">
-  <div class="mainProjectDivs">
-      <h3 class="mt-2">Fullstack Projects</h3>
-    <div class="bigBody">
-      <input type="radio" name="2position" v-for="(fullstack, index) in fullstacks" :key="fullstack.title" :id="'2position-' + index" @click="updatePosition(index)" />
-      <main id="carousel2">
-        <div class="item" v-for="(fullstack, index) in fullstacks" :key="fullstack.title" :class="{ active: index === currentPosition }">
-          <div class="projectDiv">
+    <div class="mainProjDiv">
+      <div class="mainProjectDivs">
+        <h3 class="mt-2">Fullstack Projects</h3>
+        <div class="projectGrid">
+          <div class="projectDiv" v-for="(fullstack, index) in fullstacks" :key="index">
             <div class="projectContent">
               <div class="text-center mt-2 titleP">{{ fullstack.title }}</div>
-              <img :src="`/new-portfolio${fullstack.image}`" alt="Project Image" class="" />
+              <img :src="`/new-portfolio${fullstack.image}`" alt="Project Image" class="projectImage" />
               <p class="mainP">Languages Used: <span class="mainS">{{ fullstack.language }}</span></p>
               <p class="mainP">Description: <span class="mainSA">{{ fullstack.about }}</span></p>
-              <a :href="fullstack.repo" class="" target="_blank">Github Repo</a>
+              <a :href="fullstack.repo" class="projectLink" target="_blank">Github Repo</a>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
-  </div>
+  </template>
   
-  </div>
-</template>
+  <style scoped>
+  .mainP {
+    font-weight: bold;
+    font-size: 1rem;
+    color: #333;
+  }
+  
+  .mainS {
+    font-weight: normal;
+  }
+  
+  .mainSA {
+    font-weight: normal;
+    font-size: .90rem;
+  }
+  
+  .titleP {
+    font-weight: bold;
+  }
+  
+  .projectContent a {
+    display: block;
+    text-align: center;
+    margin: 0 auto;
+  }
+  
+  .mainProjectDivs {
+    margin-top: 25px;
+    min-height: 900px;
+  }
+  
+  h3 {
+    text-align: center;
+    color: #333;
+    font-size: 1.5rem;
+  }
+  
+  img.projectImage {
+    height: 300px;
+    width: 350px; 
+  }
+  
 
-<style scoped>
-
-.mainP {
-  font-weight: bold;
-  font-size: 1rem;
-  color: #333;
-}
-
-.mainS {
-  font-weight: normal;
-  /* font-size: .90rem; */
-}
-
-.mainSA {
-  font-weight: normal;
-  font-size: .90rem;
-}
-
-.titleP {
-  font-weight: bold;
-}
-
-.projectContent a {
-  display: block;
-  text-align: center;
-  margin: 0 auto;
-}
-
-
-.mainProjDiv {
-  border: 2px solid darkgoldenrod;
-}
-
-.mainProjectDivs {
-  margin-top: 25px;
-  min-height: 900px;
-}
-
-div.item {
+  .projectGrid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); /* three projects per row */
+    gap: 20px; /* the gap between projects */
+    margin-top: 20px;
+  }
+  
+  @media (max-width: 768px) {
+    .projectGrid {
+      grid-template-columns: 1fr; /* one project per row on mobile */
+    }
+  }
+  
+  /* Styles for each project */
+  .projectDiv {
+    background-color: transparent; 
+    border: 5px solid white;
     border-radius: 25px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-h3 {
-  border: 1px dashed white;
-  text-align: center;
-  color: #333;
-  font-size: 1.5rem;
-}
-
-img {
-  height: 250px;
-  width: 300px;
-}
-
-.bigBody {
-  height: 600px;
-  margin: 0;
-  display: grid;
-  grid-template-rows: 500px 100px;
-  grid-template-columns: 1fr 30px 30px 30px 30px 30px 1fr;
-  align-items: center;
-  justify-items: center;
-}
-
-main#carousel2 {
-  grid-row: 1 / 2;
-  grid-column: 1 / 8;
-  width: 100vw;
-  height: 500px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  overflow: hidden;
-  transform-style: preserve-3d;
-  perspective: 600px;
-  --items: 13;
-  --middle: 3;
-  --position: 1;
-}
-
-div.item {
-  position: absolute;
-  width: 300px;
-  height: 400px;
-  background-color: coral;
-  --r: calc(var(--position) - var(--offset));
-  --abs: max(calc(var(--r) * -1), var(--r));
-  transition: all 0.25s linear;
-  transform: rotateY(calc(-10deg * var(--r)))
-    translateX(calc(-300px * var(--r)));
-  z-index: calc((var(--position) - var(--abs)));
-}
-
-
-div.item:nth-of-type(1) {
-  --offset: 1;
-  background-color: #90f1ef;
-}
-div.item:nth-of-type(2) {
-  --offset: 2;
-  background-color: #ff70a6;
-}
-div.item:nth-of-type(3) {
-  --offset: 3;
-  background-color: #ff9770;
-}
-div.item:nth-of-type(4) {
-  --offset: 4;
-  background-color: #ffd670;
-}
-div.item:nth-of-type(5) {
-  --offset: 5;
-  background-color: #e9ff70;
-}
-div.item:nth-of-type(6) {
-  --offset: 6;
-  background-color: #a0ff70;
-}
-
-div.item:nth-of-type(7) {
-  --offset: 7;
-  background-color: #ffcc70;
-}
-
-div.item:nth-of-type(8) {
-  --offset: 8;
-  background-color: #70ffd6; 
-}
-
-div.item:nth-of-type(9) {
-  --offset: 9;
-  background-color: #ff70e9; 
-}
-
-div.item:nth-of-type(10) {
-  --offset: 10;
-  background-color: #70a0ff;
-}
-
-input:nth-of-type(1) {
-  grid-column: 2 / 3;
-  grid-row: 2 / 3;
-}
-input:nth-of-type(1):checked ~ main#carousel2 {
-  --position: 1;
-}
-
-input:nth-of-type(2) {
-  grid-column: 3 / 4;
-  grid-row: 2 / 3;
-}
-input:nth-of-type(2):checked ~ main#carousel2 {
-  --position: 2;
-}
-
-input:nth-of-type(3) {
-  grid-column: 4 /5;
-  grid-row: 2 / 3;
-}
-input:nth-of-type(3):checked ~ main#carousel2 {
-  --position: 3;
-}
-
-input:nth-of-type(4) {
-  grid-column: 5 / 6;
-  grid-row: 2 / 3;
-}
-input:nth-of-type(4):checked ~ main#carousel2 {
-  --position: 4;
-}
-
-input:nth-of-type(5) {
-  grid-column: 6 / 7;
-  grid-row: 2 / 3;
-}
-input:nth-of-type(5):checked ~ main#carousel2 {
-  --position: 5;
-}
-
-input:nth-of-type(6) {
-  grid-column: 2 / 3;
-  grid-row: 3 / 3;
-}
-
-input:nth-of-type(6):checked ~ main#carousel2 {
-  --position: 6;
-}
-
-input:nth-of-type(7) {
-  grid-column: 3 / 4;
-  grid-row: 3 / 3;
-}
-
-input:nth-of-type(7):checked ~ main#carousel2 {
-  --position: 7;
-}
-
-input:nth-of-type(8) {
-  grid-column: 4 / 5;
-  grid-row: 3 / 3;
-}
-
-input:nth-of-type(8):checked ~ main#carousel2 {
-  --position: 8;
-}
-
-input:nth-of-type(9) {
-  grid-column: 5 / 6;
-  grid-row: 3 / 3;
-}
-
-input:nth-of-type(9):checked ~ main#carousel2 {
-  --position: 9;
-}
-
-input:nth-of-type(10) {
-  grid-column: 6 / 7;
-  grid-row: 3 / 3;
-}
-
-input:nth-of-type(10):checked ~ main#carousel2 {
-  --position: 10;
-}
-
-</style>
+    padding: 20px;
+    text-align: center;
+  }
+  
+  .projectLink {
+    color: white;
+    text-decoration: none;
+    background-color: #333;
+    padding: 10px 20px;
+    border-radius: 5px;
+    display: inline-block;
+    margin-top: 10px;
+  }
+  
+  .projectLink:hover {
+    background-color: #555;
+  }
+  </style>
+  
